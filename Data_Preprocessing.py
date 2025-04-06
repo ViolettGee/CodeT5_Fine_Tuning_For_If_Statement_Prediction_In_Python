edb77bdd82360d4472d8a86a3cbff0c55bbdc83a
Ella Bryant <egbryant01@wm.ed>
2025-04-06 13:08:48 -0400

    planned out the code for Data Processing

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -1,1 +1,31 @@
+#purpose of this file is to preprocess the data:
+# - mask each of the target if statements
+# - flatten each function
 
+#input: columns: cleaned_method, target_block, tokens_in_method
+#output: columns: flattend/masked method, target_block, tokens_in_method
+
+#import necessary libraries
+
+#function for masking the target if statement in a method
+
+#function for tokenizing the current method
+
+#main section of code iterating through the files
+
+#initialize each data frame
+
+#iterate through each data frame
+    #iterate through each row
+    
+        #call the function for masking on current row
+    
+        #call the function for tokenizing the data
+    
+        #compress the method into flattened space
+    
+        #update the tokens to correspond to the data
+    
+        #update the row of the data frame
+
+#write each data frame to a new csv
