e7e326ac6ce601535cb24681843ad037b96c5921
Ella Bryant <egbryant01@wm.ed>
2025-04-08 21:29:36 -0400

    implemented fine tuning of model

--- Model.py
+++ Model.py
@@ -2,40 +2,57 @@
 #uses evaluation data to find a stopping point
 
 #import necessary libraries
+from transformers import TrainingArguments
+from transformers import RobertaTokenizer
+from transformers import T5ForConditionalGeneration
+from transformers import DataCollatorWithPadding
+from transformers import EarlyStoppingCallback
+import pandas as pd
 
 #initialize training parameters
-
+training_args = TrainingArguments(output_dir = "Model", 
+                                  eval_strategy = "epoch", 
+                                  learning_rate = 0.05, 
+                                  load_best_model = True)
 #output_dir is the directory where model predictions and checkpoints will be written
-#do_eval is the whether or not to run evaluation on the validation set
 #eval_strategy is the type of evaluation strategy to adopt during training
-#per_device_train_batch_size is the size per device accelerator core/CPU for training
-#per_device_eval_batch_size is the size per device accelerator core/CPU for training
-#gradient_accumulation_steps is the number of updates steps to accumulate the gradients for before back propogation
-#eval_dely is the number of epochs/steps to wait for before the first evaluation can be performed
+# - epoch because I am not implementing logs
 #learning_rate is the initial learning fro the optimizer
-#weight decay is the decay to apply to all layers except bias and normalization weights in the optimizer
+# - increased from generic due to the relatively small size of the fine-tuning data set
 #load_best_model_at_end is the whether or not to load the best model found during training
-#metric_for_best_model is used in conjunction with load_bes_model_at_end to specify the model comparison metric
-# - used loss metric due to it being asked for within project specifications
-#greater_is_better is used in conjunction with model evaluation specifying which model is better
-
-#initialize pretrained model
+# - true so that selection metrics can be run for early stopping
 
 #initialize tokenizer
+tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-small')
+
+#initialize pretrained model
+model = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-small')
 
 #initialize data collator used padding due to others being built to include labels
+data_collator = DataCollatorWithPadding(tokenizer = tokenizer)
 
 #initalize callback object
-
+callbacks = EarlyStoppingCallback()
 #used early stopping due to it being asked for within the project specifications
 
 #initialize tokenized training and validation data
+training_data = pd.read_csv(columns = ['tokenized_method', 'tokenized_target'])
+validation_data = pd.read_csv(columns = ['tokenized_method', 'tokenized_target'])
 
 #initialize trainer object
-
+trainer = Trainer(model,
+                  training_args,
+                  train_dataset = training_data,
+                  eval_dataset = validation_data,
+                  data_collator = data_collator,
+                  tokenizer = tokenizer,
+                  callbacks = callbacks)
 #model is the model that is being trained, evaluated and used for productions
 #args is the training arguments that are initialized above
 #data_collator is the collator object initialized above
 #train_dataset is the dataset to use for training
 #eval_dataset is the dataset to use for evaluation
 #callbacks is the callback object initialized above
+
+#start training the model
+trainer.train()
